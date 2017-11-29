4f29711ca356aab45fef6e6c5fb8be922460416a
ykeanu <ykhernan@ucla.edu>
2017-11-29 09:21:36 -0800

    First init

---
+++ index.js
@@ -0,0 +1,16 @@
+// import { AppRegistry } from 'react-native';
+// import App from './App.js';
+
+// AppRegistry.registerComponent('integrateARGO', () => App);
+
+// // The below line is necessary for use with the TestBed App
+// AppRegistry.registerComponent('ViroSample', () => App);
+
+
+
+
+
+import { AppRegistry } from 'react-native';
+import NavigationalApp from './src/App';
+
+AppRegistry.registerComponent('ARgo', () => NavigationalApp);
